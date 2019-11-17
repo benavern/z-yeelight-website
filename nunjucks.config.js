59dafff0a813b1f52d322a215ad6961eb95f7e57
Benjamin Caradeuc <benjamin@caradeuc.info>
2019-11-18 00:40:51 +0100

    some SEO friendly stuff

--- nunjucks.config.js
+++ nunjucks.config.js
@@ -2,7 +2,9 @@ module.exports = {
   root: './src/html',
   data: {
     site: {
-      title: "Z-Yeelight"
+      title: "Z-Yeelight",
+      description: "Control your YeeLight bulbs from your computer.",
+      url: "https://z-yeelight.netlify.com/"
     }
   },
 }
